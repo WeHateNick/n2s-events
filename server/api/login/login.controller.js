7a906dd3737b1598187113b9bf5bce11417e9b25
Nick Ocampo <nick@nick-ocampo.local>
2016-11-19 17:33:48 -0800

    set login request to login endpoint

--- server/api/login/login.controller.js
+++ server/api/login/login.controller.js
@@ -5,7 +5,24 @@
 
 'use strict';
 
+import request from 'request';
+
+var reqObj = {
+  'username': 'nicolasocampo89@hotmail.com',
+  'password': 'Simple123'
+};
+
 // Gets a list of Logins
 export function index(req, res) {
-  res.json([]);
+	request.post(
+    'https://n2sreno.clubspeedtiming.com/api/index.php/customers/login?key=7VlopsPCbJeotMiXdCH4',
+    { json: reqObj },
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        console.log('NEED 2 SPEED:');
+        console.log(body);
+				res.json(body);
+      }
+    }
+	);
 }
--- server/api/thing/thing.controller.js
+++ server/api/thing/thing.controller.js
@@ -6,28 +6,8 @@
 'use strict';
 
 // Gets a list of Things
-var request = require('request');
 
 export function index(req, res) {
-	request('http://www.google.com', function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-	    console.log('It works!!') // Show the HTML for the Google homepage.
-	  }
-	})
-	var reqObj = {
-	  'username': 'nicolasocampo89@hotmail.com',
-	  'password': 'Simple123'
-	};
-	request.post(
-	    'https://n2sreno.clubspeedtiming.com/api/index.php/customers/login?key=7VlopsPCbJeotMiXdCH4',
-	    { json: reqObj },
-	    function (error, response, body) {
-	        if (!error && response.statusCode == 200) {
-	            console.log('NEED 2 SPEED:');
-	            console.log(body);
-	        }
-	    }
-	);
   res.json([
   	{
   		name: 'Pepperoni',
