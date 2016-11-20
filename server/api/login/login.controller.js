8981d1e81a2e7b683a728a837302dc41286adf86
Nick Ocampo <nick@wehatenick.com>
2016-11-19 17:40:34 -0800

    refactor

--- server/api/login/login.controller.js
+++ server/api/login/login.controller.js
@@ -11,17 +11,24 @@ var reqObj = {
   'username': 'nicolasocampo89@hotmail.com',
   'password': 'Simple123'
 };
+var baseUrl = 'https://n2sreno.clubspeedtiming.com/api/index.php/';
+var endpoint = 'customers/login/';
+const N2S_AUTH_KEY = '7VlopsPCbJeotMiXdCH4';
 
 // Gets a list of Logins
 export function index(req, res) {
 	request.post(
-    'https://n2sreno.clubspeedtiming.com/api/index.php/customers/login?key=7VlopsPCbJeotMiXdCH4',
+    baseUrl + endpoint + '?key=' + N2S_AUTH_KEY,
     { json: reqObj },
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
         console.log('NEED 2 SPEED:');
         console.log(body);
 				res.json(body);
+      }
+      else {
+        console.log('== Login POST Error');
+        console.log(error);
       }
     }
 	);
